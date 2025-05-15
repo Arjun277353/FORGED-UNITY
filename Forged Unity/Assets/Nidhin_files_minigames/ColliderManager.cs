3007d11308765bab215a9e194fd78116d75fc734
nxdhin <gnidhinraj@gmail.com>
2025-05-15 15:43:53 +0530

    Finished Forging Document Minigame
    Finalized version with drawing and checkbox mechanics and the button added

--- Forged Unity/Assets/Nidhin_files_minigames/ColliderManager.cs
+++ Forged Unity/Assets/Nidhin_files_minigames/ColliderManager.cs
@@ -1,65 +1,82 @@
 using UnityEngine;
 
-public class HitboxCheck : MonoBehaviour
+public class HitboxCollision : MonoBehaviour
 {
     public BoxCollider2D checkbox1;
+    public GameObject tick1;
+
     public BoxCollider2D checkbox2;
+    public GameObject tick2;
+
     public BoxCollider2D checkbox3;
+    public GameObject button;
+
+    private float holdTime = 0f;
+    private bool checkbox1Triggered = false;
+    private bool checkbox2Triggered = false;
+    private bool checkbox3Triggered = false;
+
+    public bool allCheckboxesChecked => checkbox1Triggered && checkbox2Triggered && checkbox3Triggered;
+
+    private void Start()
+    {
+        tick1.SetActive(false);
+        tick2.SetActive(false);
+    }
 
     void Update()
     {
+        Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
         if (Input.GetMouseButtonDown(0))
         {
-            Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            // Draw debug ray in Scene view
-            Debug.DrawRay(mouseWorldPos, Vector3.forward * 0.1f, Color.yellow, 1f);
-
-            // Check all hit colliders at this point
-            Collider2D[] hits = Physics2D.OverlapPointAll(mouseWorldPos);
-
-            if (hits.Length == 0)
+            if (checkbox1.OverlapPoint(mouseWorldPos))
             {
-                Debug.Log("No collider hit");
+                Debug.Log("Checkbox 1 clicked\nEnabling TickBox 1");
+                tick1.SetActive(true);
+                checkbox1Triggered = true;
             }
-            else
+
+            else if (checkbox2.OverlapPoint(mouseWorldPos))
             {
-                foreach (Collider2D col in hits)
+                Debug.Log("Checkbox 2 clicked\nEnabling TickBox 2");
+                tick2.SetActive(true);
+                checkbox2Triggered = true;
+            }
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            if (checkbox3.OverlapPoint(mouseWorldPos))
+            {
+                holdTime += Time.deltaTime;
+
+                if (!checkbox3Triggered && holdTime >= 3f)
                 {
-                    Debug.Log("Hit: " + col.name);
+                    checkbox3Triggered = true;
+                    Debug.Log("Checkbox 3 held for 3 seconds!");
+                    OnCheckbox3HoldComplete();
                 }
             }
-
-            // Optional direct checks (if still needed for logic flow)
-            if (checkbox1.OverlapPoint(mouseWorldPos))
-                Debug.Log("Drawing inside checkbox 1");
-            else if (checkbox2.OverlapPoint(mouseWorldPos))
-                Debug.Log("Drawing inside checkbox 2");
-            else if (checkbox3.OverlapPoint(mouseWorldPos))
-                Debug.Log("Drawing inside checkbox 3");
             else
-                Debug.Log("Not inside any checkbox");
+            {
+                holdTime = 0f;
+            }
+        }
+        else
+        {
+            holdTime = 0f;
+        }
+
+        if (allCheckboxesChecked)
+        {
+            Debug.Log("All checkboxes are triggered!");
         }
     }
 
-    void OnDrawGizmos()
+    void OnCheckbox3HoldComplete()
     {
-        if (checkbox1 != null)
-        {
-            Gizmos.color = Color.green;
-            Gizmos.DrawWireCube(checkbox1.bounds.center, checkbox1.bounds.size);
-        }
-
-        if (checkbox2 != null)
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireCube(checkbox2.bounds.center, checkbox2.bounds.size);
-        }
-
-        if (checkbox3 != null)
-        {
-            Gizmos.color = Color.black;
-            Gizmos.DrawWireCube(checkbox3.bounds.center, checkbox3.bounds.size);
-        }
+        Debug.Log("Function triggered after holding Checkbox 3");
+        button.SetActive(true);
     }
 }
